c8e8af39ce21d57a9f79b5f82709fb1635d4d131
Erwin Adorable <adorable.erwin@gmail.com>
2024-01-15 23:15:48 +0100

    created billing for defined number of hours played

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@
 Rails.application.routes.draw do
   root to: 'parents#welcome'
   resources :parents do
-    resources :plays do
-
+    resources :plays, only: [:new, :create, :update, :edit, :destroy] do
     end
   end
+  # get 'index', to: 'playss#index'
+  # get 'show', to: 'plays#show'
+  resources :plays, only: [:index, :show]
   get 'thank_you', to: 'parents#thank_you'
   get 'welcome', to: 'parents#welcome'
 
